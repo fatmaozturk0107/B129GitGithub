0f347c7aa626bf980607882935f69c54f7bf5749
Fatma Öztürk <candagasan2016@gmail.com>
2023-02-25 19:28:45 +0300

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,6 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("dunya");
+        System.out.println("b129");
 
     }
 }
